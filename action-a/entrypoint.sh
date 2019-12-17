996039022bb74cc99fa1311eaa2bb6e98df1dfe3
Sandesh Shetty <sandeshs@devtools.in>
2019-12-17 12:42:38 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $MY_NAME"
-sh -c "sudo apt-get install git -y"
+sh -c "apt-get install git -y"
 sh -c "git clone https://github.com/sandesh2026/NewActionRepo.git"
 sh -c "cd NewActionRepo"
 sh -c "echo adding dummy content >> dummyfile"
