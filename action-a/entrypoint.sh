6f07f89f909ac0cd0304e4285148d50c66cbf4a4
Sandesh Shetty <sandeshs@devtools.in>
2019-12-17 12:39:26 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $MY_NAME"
+sh -c "git clone https://github.com/sandesh2026/NewActionRepo.git"
+sh -c "cd NewActionRepo"
+sh -c "echo adding dummy content >> dummyfile"
+sh -c "git commit -a -m "new commit""
+sh -c "git push origin master"
 
 
 
